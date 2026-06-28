483221be026b526cbd116a9f6d5e69269a78d950
Roast Fragrance <fyfisius@gmail.com>
2026-06-28 19:28:55 -0400

    small changes to config.ts and astro.config.mjs to update profile information and site settings. zh-CN test

--- src/config.ts
+++ src/config.ts
@@ -54,7 +54,7 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Kokoaki Unzogawa",
+	name: "蕰藻川心秋",
 	bio: "Zhi Wo Zui Wo, Qi Wei Chun Qiu", // A short description of yourself, can be left empty
 	links: [
 		{
