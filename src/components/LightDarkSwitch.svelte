ae54f5e01350ad78d821177e8cad10db07c3e4a1
www1ball1n <fyfisius@gmail.com>
2026-07-15 23:29:50 -0400

    simplifying workflow

--- src/components/LightDarkSwitch.svelte
+++ src/components/LightDarkSwitch.svelte
@@ -11,6 +11,12 @@ import {
 import { onMount } from "svelte";
 import type { LIGHT_DARK_MODE } from "@/types/config.ts";
 
+interface Props {
+	[key: string]: unknown;
+}
+
+let { ...rest }: Props = $props();
+
 const seq: LIGHT_DARK_MODE[] = [LIGHT_MODE, DARK_MODE, AUTO_MODE];
 let mode: LIGHT_DARK_MODE = $state(AUTO_MODE);
 
